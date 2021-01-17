315c49d9b2a67cab722b5f189ff0a09c2d217e0f
Adebayo <adebayotobiloba9@gmail.com>
2021-01-17 04:12:25 +0100

    persisted to local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,16 @@
 import { combineReducers } from "redux";
 import nomineeReducer from "./nominated/nominated.reducer";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["nominee"],
+};
+
+const rootReducer = combineReducers({
   nominee: nomineeReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
